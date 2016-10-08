0d6094832d4124bf126e40afa302535812b86532
nikolavrbanic <nikola.vrbanic@gmail.com>
2016-10-08 17:04:25 +0200

    NeuralNetworks nest sitnog

--- pack2/NeuralNetwork.java
+++ pack2/NeuralNetwork.java
@@ -1,8 +1,34 @@
 package pack2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NeuralNetwork {
-
-
+	List<Layer> layerList = new ArrayList<>();
+	
+	public NeuralNetwork(Layer ...layers){
+		
+		for(Layer layer : layers){
+			layerList.add(layer);
+		}
+		connectLayers();		
+	}
+	
+	public NeuralNetwork(List<Layer> layers){
+		layerList.addAll(layers);
+		connectLayers();
+	}
+	
+	private void connectLayers(){
+		for(int i = 0; i< layerList.size(); i++){
+			layerList.get(i).connect(layerList.get(i+1));
+		}
+	}
+	
+	public double[] run(double[] inputs){
+		return inputs;
+		
+	}
 	//The magic happens here
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
