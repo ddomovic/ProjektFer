c5caa62b1da30ae8cec65feca15bb23b57a48736
jub36 <jub36@openmailbox.org>
2016-10-14 12:44:16 +0200

    Transfer function po layeru

---
+++ pack2/LinearTransferFunction.java
@@ -0,0 +1,15 @@
+package pack2;
+
+/**
+ * Aktivacijska funkcija linear neurona.
+ *
+ * @version 1.0
+ */
+public class LinearTransferFunction implements ITransferFunction {
+
+	@Override
+	public double applyFunction(double z) {
+		return z;
+	}
+
+}
--- pack2/NeuralNetwork.java
+++ pack2/NeuralNetwork.java
@@ -24,22 +24,8 @@ public class NeuralNetwork {
 	 *
 	 * @param tfunction transferna funkcija
 	 */
-	public NeuralNetwork(ITransferFunction tfunction) {
+	public NeuralNetwork() {
 		this.layerList = new ArrayList<>();
-		this.tfunction = tfunction;
-	}
-
-	/**
-	 * Inicijalizira neuronsku mrežu s danom funkcijom i layerima s veličinama danim u drugom argumentu.
-	 *
-	 * @param tfunction transferna funkcija
-	 * @param layerSizes velicine layera
-	 */
-	public NeuralNetwork(ITransferFunction tfunction, int ... layerSizes) {
-		this(tfunction);
-		for (int i : layerSizes) {
-			this.addLayer(i);
-		}
 	}
 
 	/**
@@ -48,7 +34,7 @@ public class NeuralNetwork {
 	 *
 	 * @param numberOfNeurons velicina novog layera
 	 */
-	public void addLayer(int numberOfNeurons) {
+	public void addLayer(int numberOfNeurons, ITransferFunction tfunction) {
 		Layer layer = new Layer(numberOfNeurons, tfunction);
 		//ako nije prvi layer onda ga spoji sa skroz desnim(zadnjim) layerom
 		if (!layerList.isEmpty()) {
@@ -88,11 +74,12 @@ public class NeuralNetwork {
 	 * @param args parametri komandne linije
 	 */
 	public static void main(String[] args) {
-		ITransferFunction tfunction = new SigmoidTransferFunction();
-		NeuralNetwork network = new NeuralNetwork(tfunction);
-		network.addLayer(3);
-		network.addLayer(4);
-		network.addLayer(2);
+		ITransferFunction tsigmoid = new SigmoidTransferFunction();
+		ITransferFunction tlinear = new LinearTransferFunction();
+		NeuralNetwork network = new NeuralNetwork();
+		network.addLayer(3, tsigmoid);
+		network.addLayer(4, tsigmoid);
+		network.addLayer(2, tlinear);
 
 		double[] input = {7.0,16.0,3.0};
 		double[] output = network.run(input);
