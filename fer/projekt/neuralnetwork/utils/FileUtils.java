87b9823369b0faad090453886c9b90e03480d57b
davidpodrebarac <david.podrebarac@fer.hr>
2016-12-01 17:43:37 +0100

    Rijeseno spremanje i loadanje networka iz fileova

--- fer/projekt/neuralnetwork/utils/FileUtils.java
+++ fer/projekt/neuralnetwork/utils/FileUtils.java
@@ -19,19 +19,24 @@ import fer.projekt.neuralnetwork.regression.LinearRegressionNetwork;
 
 public class FileUtils {
 
-	private static String getLayerDescription(Layer layer) {
+	private static String getLayerDescription(int layerIndex, Layer layer) {
 		StringBuilder sb = new StringBuilder();
-		sb.append("layerIndex=" + layer.getIndex() + System.lineSeparator());
+		sb.append("layerIndex=" + layerIndex + System.lineSeparator());
+		
 		layer.forEach(new Consumer<Neuron>() {
-			int index = 0;
-
+			int nIndex = 0;
 			@Override
 			public void accept(Neuron n) {
-				sb.append("nindex=" + index++ + " bias=" + n.getBias() + System.lineSeparator());
+				sb.append("nIndex=" + nIndex++ + " bias=" + n.getBias() + System.lineSeparator());
+				
 				List<Connection> connections = n.getAllInConnections();
-				connections.forEach(c -> {
-					sb.append("cindex=" + c.getId() + " weight=" + c.getWeight() + System.lineSeparator());
-				});				
+				connections.forEach(new Consumer<Connection>() {
+					int cIndex = 0;
+					@Override
+					public void accept(Connection c) {
+						sb.append("cIndex=" + cIndex++ + " weight=" + c.getWeight() + System.lineSeparator());
+					}
+				});
 			}
 		});
 		return sb.toString();
@@ -42,11 +47,25 @@ public class FileUtils {
 		try (BufferedWriter bw = Files.newBufferedWriter(p, StandardCharsets.UTF_8)) {
 			ArrayList<Layer> layerList = network.getLayerList();
 			layerList.forEach(l -> {
-				String layerDescription = FileUtils.getLayerDescription(l);
 				try {
-					bw.write(layerDescription);
+					bw.write(l.size() + ",");
 				} catch (IOException e) {
 					e.printStackTrace();
+				}
+			});
+			bw.newLine();
+			
+			layerList.forEach(new Consumer<Layer>() {
+				int layerIndex = 0;
+				@Override
+				public void accept(Layer l) {
+					String layerDescription = FileUtils.getLayerDescription(layerIndex, l);
+					layerIndex++;
+					try {
+						bw.write(layerDescription);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}					
 				}
 			});
 		} catch (IOException e) {
@@ -61,26 +80,37 @@ public class FileUtils {
 		Neuron n = null;
 		try (BufferedReader br = Files.newBufferedReader(p, StandardCharsets.UTF_8)) {
 			String line = null;
+			if (br.ready()) {
+				line = br.readLine();
+			}
+			//provjera je su li layeri iste velicine
+			StringBuffer sb = new StringBuffer();
+			ArrayList<Layer> layerList = network.getLayerList();
+			layerList.forEach(layer -> {
+				sb.append(layer.size() + ",");
+			});
+			if (!line.equals(sb.toString()) && line != null) {
+				throw new IllegalArgumentException("Broj layera ili broj neurona u njima ne odgovara definiciji networka u file-u!" +
+						"\nTraženi broj layera je: " + line + " a dobiveni: " + sb.toString());						
+			}
+			
 			while (br.ready() && (line = br.readLine()) != null) {
 				if (line.startsWith("layerIndex")) {
-					String index = line.substring(line.indexOf("=") + 1);
-					l = network.getLayerList().get(Integer.valueOf(index));
-				} else if (line.startsWith("nid")) {
-					String nid = line.substring(line.indexOf("=") + 1, line.indexOf(" "));
-					n = l.getNeuron(Integer.valueOf(nid));
+					String layerIndex = line.substring(line.indexOf("=") + 1);
+					l = network.getLayerList().get(Integer.valueOf(layerIndex));
+				} else if (line.startsWith("nIndex")) {
+					String nIndex = line.substring(line.indexOf("=") + 1, line.indexOf(" "));
+					n = l.getNeuron(Integer.valueOf(nIndex));
 					
 					String bias = line.substring(line.indexOf("bias=") + 5);
 					n.setBias(Double.valueOf(bias));
-				} else if (line.startsWith("cid")) {
+				} else if (line.startsWith("cIndex")) {
 					ArrayList<Connection> inConnections = n.getAllInConnections();
 					
-					int cid = Integer.valueOf(line.substring(line.indexOf("=") + 1, line.indexOf(" ")));
+					int cIndex = Integer.valueOf(line.substring(line.indexOf("=") + 1, line.indexOf(" ")));
 					String weight = line.substring(line.indexOf("weight=") + 7);
-					inConnections.forEach(c -> {
-						if (c.getId() == cid) {
-							c.setWeight(Integer.valueOf(weight));
-						}
-					});
+					Connection c = inConnections.get(cIndex);
+					c.setWeight(Double.valueOf(weight));
 				}
 			}
 		} catch (IOException e) {
