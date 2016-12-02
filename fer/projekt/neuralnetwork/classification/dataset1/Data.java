96f70569472fc3aa51ff940a2192a2b25583191c
nikolavrbanic <nikola.vrbanic@gmail.com>
2016-12-02 11:50:03 +0100

    osnovne varijable i geteri i setteri za dataset

--- fer/projekt/neuralnetwork/classification/dataset1/Data.java
+++ fer/projekt/neuralnetwork/classification/dataset1/Data.java
@@ -1,5 +1,28 @@
 package fer.projekt.neuralnetwork.classification.dataset1;
 
 public class Data {
+	/**
+	 *Temperatura pacijenta
+	 */
+	private double Temperature;
+	/**
+	 * Da li je pacijentu muka/ ima li mučninu?
+	 * 1 == da, 0 == ne
+	 */
+	private int nausea;
+	/**
+	 * bol u donjem dijelu leđa?
+	 * 1 == da, 0 == ne
+	 */
+	private int lumbPain;
+	/**
+	 * Stalna potreba za pišanjem?
+	 * 1 == da, 0 == ne
+	 */
+	private int urinePush;
 
+	private int mictPain;
+	private int burn;
+	private int inflammation;
+	private int nephrihis;
 }
